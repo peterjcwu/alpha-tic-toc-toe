d91a6d042e155cece352d425446a57be21fc5394
peterjcwu <wolf952@gmail.com>
2025-04-07 15:46:01 +0800

    add files

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/static/<path:path>')
+def static_file(path):
+    return app.send_static_file(f"./static/{path}")
